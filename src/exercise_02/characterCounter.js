b01dbcaaa8f80e01c751c9e5b70d9b91160c71a8
Doris8462 <liandou123>
2020-07-21 17:44:47 +0800

    [huangyunjie] feat:exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,9 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let count = 0;
+  if (string === null || string === undefined || string.length === 0);
+  else if (prediction) count = string.match(/l/ig).length;
+  else count = string.match(/[a-z]/ig).length;
+  return count;
 }
