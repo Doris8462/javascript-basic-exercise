877911ccb2ddbd93e60e7ca770d907685ecd967e
Doris8462 <liandou123>
2020-07-21 15:21:56 +0800

    [huangyunjie] feat:exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,17 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  
+  if (array === null || array === undefined) throw ('Flatten undefined or null array');
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        result.push(array[i][j]);
+      }
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
