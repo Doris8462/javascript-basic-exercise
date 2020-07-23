9636e0c3f55c86ed83a42360fc71c2fef21bd763
Doris8462 <liandou123>
2020-07-23 11:16:15 +0800

    [huangyunjie] feat:exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,34 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  function isPromise(obj) {
+    return (!!obj && (typeof obj === 'object' || typeof obj === 'function')
+      && typeof obj.then === 'function'
+    );
+  }
+
+  for (let p of promises) {
+    if (!isPromise(p)) throw new Error('Not all elements are promises.');
+  }
+
+  const rejectArr = [];
+  const arr = [];
+  let i = 0;
+  for (let p of promises) {
+    let idx = i;
+    rejectArr.push(false);
+    arr.push(
+      new Promise((resolve) => resolve(p)).catch((err) => {
+        rejectArr[idx] = true;
+      })
+    );
+    i++;
+  }
+
+  return Promise.all(arr).then((value) => {
+    let idx_rej = rejectArr.indexOf(true);
+    if (idx_rej >= 0) {
+      throw new Error('It should not be a success promise');
+    }
+  });
 }
