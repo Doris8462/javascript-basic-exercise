d65951b5f219523b418c76cfb65a88b5570fd050
Doris8462 <liandou123>
2020-07-24 10:38:28 +0800

    [huangyunjie] feat:exercise_06

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -11,6 +11,51 @@ export default class Vector {
   // * Please implement the class and pass all the tests in vector_spec.js.
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;  
+  }
+}
+  Vector.prototype={
+  constructor: Vector,
+  flagx : false,
+  flagy : false,
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  get x() {
+  return this._x;
+},
+  set x(newx) {
+  if(!flagx) {
+  this._x=newx;
+  flayx=true;
+}
+else throw 'should not set x of the vector';
+},
+
+  get y() {
+  return this._y;
+},
+  set y(newy) {
+  if(!flagy) {
+  this._y=newy;
+  flagy=true;
+}
+else throw 'should not set y of the vector';
+},
+
+  distance: function() {
+  return Math.sqrt(this._x*this._x+this._y*this._y);
+}
+}
+  Vector.plus=function (v1, v2) {
+  var result= new Object();
+  result.x=v1.x+v2.x;
+  result.y=v1.y+v2.y;
+  return result;
+}
+  Vector.minus=function (v1, v2) {
+  var result= new Object();
+  result.x=v1.x-v2.x;
+  result.y=v1.y-v2.y;
+  return result;
 }
